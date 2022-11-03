e0fa219ecae2eee57c61cad5510faa12c2b54dff
czf <505487452@qq.com>
2022-11-03 14:53:36 +0800

    deploy

---
+++ precache-manifest.5cfdd8371be5871edbd0b692b7010644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3df9a998343a5278df",
+    "url": "/Account/css/app.ec620d6b.css"
+  },
+  {
+    "revision": "5e57ccb35a1651f0ce9ddbe1a93af729",
+    "url": "/Account/index.html"
+  },
+  {
+    "revision": "1f3df9a998343a5278df",
+    "url": "/Account/js/app.c312f76f.js"
+  },
+  {
+    "revision": "cf32f24d2bfbe6fa728e",
+    "url": "/Account/js/chunk-vendors.e0d9ef9e.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/Account/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Account/robots.txt"
+  }
+]);
